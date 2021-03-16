b9682db5df40738a272b4fe2fc0eea354bd89ff2
keplerridge <kepler.ridge@gmail.com>
2021-03-16 13:43:48 -0600

    added authentication enpoints

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const express = require('express'),
       app = express(),
       massive = require('massive'),
       session = require('express-session'),
+      authOwnerCtrl = requie('./controllers/authOwnerController.js'),
+      authUserCtrl = requie('./controllers/authUserController.'),
       {SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env;
 
 app.use(express.json());
@@ -23,3 +25,10 @@ massive({
     app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`))
     console.log('DB Connected')
 });
+
+//Authentication Owner Endpoints
+app.post('/auth/registerowner', authOwnerCtrl.register);
+app.post('/auth/ownerlogin', authOwnerCtrl.login);
+app.get('/auth/ownerlogout', authOwnerCtrl.logout);
+
+//Authentication User Enpoints
